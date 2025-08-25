ed8b8a69c91d0da0a4d73bd55f79fa12909ace28
Rodrigo Motta <rodrigormotta7@gmail.com>
2025-08-25 18:59:27 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,7 +1,7 @@
 pessoa = {"nome": "Rodrigo", "idade": 19, "cidade": "RJ"}
-print(pessoa.keys())
-print(pessoa.values())
-print(pessoa.items())
+print(pessoa.keys())   # Imprime dict_keys(["nome", "idade", "cidade"])
+print(pessoa.values())   # Imprime dict_values(["Rodrigo", 19, "RJ"])
+print(pessoa.items())    # Imprime dict_items([("nome", "Rodrigo"), ("idade", 19), ("cidade", "RJ")])
 
 pessoa.update({"ocupação": "Universitário"})
-print(pessoa.values())
+print(pessoa.values())    # Imprime {"nome": "Rodrigo", "idade": 19, "cidade": "RJ", "ocupação": "Universitário"}
