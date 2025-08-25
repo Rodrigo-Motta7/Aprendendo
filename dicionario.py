4e7d61172c2bd53b7eaff8c547022888098c4e44
Rodrigo Motta <rodrigormotta7@gmail.com>
2025-08-25 18:51:35 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,7 @@
+pessoa = {"nome": "Rodrigo", "idade": 19, "cidade": "RJ"}
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+
+pessoa.update({"ocupação": "Universitário"})
+print(pessoa.values())
