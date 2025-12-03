61fe19c32eed5981d685923fa2e6424556f66d19
Rodrigo Motta <rodrigormotta7@gmail.com>
2025-12-03 16:11:23 -0300

    Add files via upload

---
+++ num_primo.py
@@ -0,0 +1,13 @@
+def eh_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero**0.5) + 1 ):
+        if numero % i ==0:
+            return False
+    return True
+    
+num= int(input("Digite um número para verificar se é primo:"))
+if eh_primo(num):
+    print(f"o número {num} é primo")
+else:
+    print(f"o numero {num} não é primo")
