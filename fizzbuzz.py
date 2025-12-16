d1e32aa875dc3d78975a624d2c4c39f911f33b8b
Rodrigo Motta <rodrigormotta7@gmail.com>
2025-12-16 17:54:39 -0300

    fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,7 @@ for i in range(1, 51):
     if i % 3 == 0 and i % 5 == 0:
         print(f"{i} é FizzBuzz")
     elif i % 3 == 0:
-        print (f"{i} é Frizz")
+        print (f"{i} é Fizz")
     elif i % 5 == 0:
+
         print(f"{i} é Buzz")
