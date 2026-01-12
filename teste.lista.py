da0be03e8d4500485bd5d22a763424634f9ddd04
Rodrigo Motta <rodrigormotta7@gmail.com>
2026-01-12 15:30:32 -0300

    Add files via upload

---
+++ teste.lista.py
@@ -0,0 +1,11 @@
+compras = ['ovo', 'arroz', 'frango'] #cria a lista
+compras.append('aveia') #adiciona um item ao ultino lugar da lista
+compras.insert(2,'banana') #adiciona um item a posição desejada
+compras.pop #remove o ultimo item da lista
+
+letras = ['a', 'b', 'c', 'd', 'e'] #cria uma lista com 5 elementos
+tamanho = len(letras) #armazena o tamanho atual da em uma variável
+print('Tamanho da lista:', tamanho) #imprime o tamanho da lista
+
+print(compras)
+print('tamanho da lista de compras:', len(compras)) #imprime o tamanho da lista
