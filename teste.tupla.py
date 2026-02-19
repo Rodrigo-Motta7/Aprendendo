07493e9d3e9aa8e3ad6ebc0ac958971fdd3f061c
Rodrigo Motta <rodrigormotta7@gmail.com>
2026-02-19 17:10:59 -0300

    Add files via upload

---
+++ teste.tupla.py
@@ -0,0 +1,5 @@
+#tuplas sâo imutaveis
+frutas = ("banana", "manga", "maca",) #elas são criadas com parênteses e terminam com virgula
+
+print(frutas[0]) #imprime a primeira posição
+print(len(frutas)) #imprime o número de itens na tupla
