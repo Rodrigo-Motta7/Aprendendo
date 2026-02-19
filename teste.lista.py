9b7f6db75b29ce5208f0b750e7af5280fd785a9e
Rodrigo Motta <rodrigormotta7@gmail.com>
2026-02-19 16:56:26 -0300

    Update teste.lista.py
    Added a remove method for the 'ovo' item and printed the size of the compras list.

--- teste.lista.py
+++ teste.lista.py
@@ -1,11 +1,13 @@
 compras = ['ovo', 'arroz', 'frango'] #cria a lista
 compras.append('aveia') #adiciona um item ao ultino lugar da lista
 compras.insert(2,'banana') #adiciona um item a posição desejada
-compras.pop #remove o ultimo item da lista
+compras.pop() #remove o ultimo item da lista
+compras.remove("ovo") #remove da lista o item que você escrever(se tiver repetido remove o primeiro)
 
 letras = ['a', 'b', 'c', 'd', 'e'] #cria uma lista com 5 elementos
 tamanho = len(letras) #armazena o tamanho atual da em uma variável
 print('Tamanho da lista:', tamanho) #imprime o tamanho da lista
 
 print(compras)
+
 print('tamanho da lista de compras:', len(compras)) #imprime o tamanho da lista
